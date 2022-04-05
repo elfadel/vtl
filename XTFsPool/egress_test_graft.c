79a3eba03bd09bea81fac8b781129b2654036d7c
efbonfoh <bonfohelfadel@gmail.com>
2022-04-05 20:06:53 +0200

    Comprehensive instructions to run VTL

--- XTFsPool/egress_test_graft.c
+++ XTFsPool/egress_test_graft.c
@@ -1,14 +1,10 @@
-//EGRESS GOBACKN ARQ GRAFT
+// ### EGRESS CANO GRAFT
 
 #include <stdbool.h>
 #include <stdint.h>
-#include <stdlib.h>
-#include <unistd.h>
-
 #include <linux/if_ether.h>
 #include <netinet/ip.h>
-#include <linux/pkt_cls.h>
-
+#include <linux/pkt_cls.h> //TODO: add it to headers repo ?
 #include <linux/bpf.h>
 
 #include "bpf/bpf_helpers.h"
@@ -16,232 +12,116 @@
 
 #include "../include/vtl.h"
 
-#define RETX_MAX                10
-#define WND_SIZE                16
-
-struct bpf_elf_map SEC("maps") ACK_WND_MAP = {
-        .type        = BPF_MAP_TYPE_ARRAY,
-        .size_key    = sizeof(unsigned int),
-        .size_value  = sizeof(struct sock_state_t),
-        .pinning     = PIN_GLOBAL_NS,
-        .max_elem    = WND_SIZE,
+struct bpf_elf_map SEC("maps") QOS_NEGO_MAP = {
+    .type = BPF_MAP_TYPE_ARRAY,
+    .size_key = sizeof(unsigned int),
+    .size_value = sizeof(negotiation_state),
+    .pinning = PIN_GLOBAL_NS,
+    .max_elem = 1, // Should be > 1 to handle multi applis
 };
 
-struct bpf_elf_map SEC("maps") NUM_SEQ_MAP = {
-        .type        = BPF_MAP_TYPE_ARRAY,
-        .size_key    = sizeof(unsigned int),
-        .size_value  = sizeof(uint16_t),
-        .pinning     = PIN_GLOBAL_NS,
-        .max_elem    = 1,
-};
+#define RETX_MAX      10
 
-struct bpf_elf_map SEC("maps") BUF_WND_MAP = {
-        .type        = BPF_MAP_TYPE_ARRAY,
-        .size_key    = sizeof(unsigned int),
-        .size_value  = sizeof(vtl_pkt_t),
-        .pinning     = PIN_GLOBAL_NS,
-        .max_elem    = WND_SIZE,
-};
+/* TODO: Move to vtl.h 
+ * This function move pointer to the first field of the VTL header pkt
+ * If the pkt is not VTL, it will return NULL
+*/
+static __always_inline vtl_hdr_t* egress_move_to_vtl_hdr(struct __sk_buff *skb) {
+    void *data = (void *)(long)skb->data;
+    void *data_end = (void *)(long)skb->data_end;
 
-struct bpf_elf_map SEC("maps") LEN_WND_MAP = {
-        .type           = BPF_MAP_TYPE_ARRAY,
-        .size_key       = sizeof(unsigned int),
-        .size_value     = sizeof(uint16_t),
-        .pinning        = PIN_GLOBAL_NS,
-        .max_elem       = WND_SIZE, 
-};
+  struct ethhdr *eth = (struct ethhdr *)data;
+    if(eth + 1 > data_end){
+        bpf_printk("ETH layer: malformed header.\n");
+        return NULL;
+    }
+
+    struct iphdr *iph = (struct iphdr *)(eth + 1);
+    if(iph + 1 > data_end){
+        bpf_printk("IP layer: malformed header.\n");
+        return NULL;
+    }
+
+    if(iph->protocol != IPPROTO_VTL)
+        return NULL;
+
+    vtl_hdr_t *hdr = (vtl_hdr_t *)(iph + 1);
+    if(hdr + 1 > data_end){
+        bpf_printk("VTL layer: malformed header.\n");
+        return NULL;
+    }
+
+    return hdr;
+}
 
 SEC("egress_tf_sec")
 int _tf_tc_egress(struct __sk_buff *skb) {
-
-        void *data = (void *)(long)skb->data;
-        void *data_end = (void *)(long)skb->data_end;
-
-        struct ethhdr *eth = (struct ethhdr *)data;
-        if(eth + 1 > data_end)
-                return TC_ACT_OK;
-
-        struct iphdr *iph = (struct iphdr *)(eth + 1);
-        if(iph + 1 > data_end)
-                return TC_ACT_OK;
-
-        if(iph->protocol != IPPROTO_VTL)
-                return TC_ACT_OK;
-
-        vtl_hdr_t *vtlh = (vtl_hdr_t *)(iph + 1);
-        if(vtlh + 1 > data_end)
-                return TC_ACT_OK;
-        
-        struct sock_state_t *sk_state = NULL;
-        vtl_pkt_t *save_skb = NULL;
-        uint16_t save_len = 0;
-        
-        int id_seq = 0, id_ack_buf = 0;
-        const uint16_t last_len = (const uint16_t)skb->len;
-        uint16_t *num_seq = NULL;
-
-        // Filling VTL packet header: start
-
+  
+    vtl_hdr_t *vtlh = egress_move_to_vtl_hdr(skb);
+    if(!vtlh)
+        return TC_ACT_OK;
+  
+    if(vtlh->gid == -1) {
+        bpf_printk("gid = %d ==> this is a DATA.\n", vtlh->gid);
+        bpf_printk("pkt send to nic = %d\n", skb->ifindex+1);
         vtlh->pkt_type = DATA;
-        
-        uint16_t sum = 0;
-        if(vtl_csum(data, data_end, &sum) == -1)
-                return TC_ACT_OK;
-        vtlh->checksum = sum;
+        bpf_vtl_nic_tx(skb, skb->ifindex+1, 0, 0);
+    } 
+    else { // Assumption: VTL present only two types of packets at egress path till now
+        bpf_printk("gid = %d ==> this is a NEGO.\n", vtlh->gid);
+        vtlh->pkt_type = NEGO;
+        bpf_vtl_nic_tx(skb, skb->ifindex+1, 0, 0);
+    }
 
-        num_seq = (uint16_t *) bpf_map_lookup_elem(&NUM_SEQ_MAP, &id_seq);
-        if(!num_seq)
-                return TC_ACT_OK;
-        vtlh->seq_num = *num_seq;
-
-        // Filling VTL packet header: end
-
-        id_ack_buf = (unsigned int) *num_seq;
-        sk_state = (struct sock_state_t *) bpf_map_lookup_elem(&ACK_WND_MAP, &id_ack_buf);
-        if(!sk_state)
-                return TC_ACT_OK;
-        sk_state->event = IDLE;
-        bpf_map_update_elem(&ACK_WND_MAP, &id_ack_buf, sk_state, BPF_ANY);
-
-        uint32_t cur_pload_len = (uint32_t) vtlh->payload_len;
-        if(cur_pload_len !=  VTL_DATA_SIZE) {
-                save_len = skb->len;
-                bpf_map_update_elem(&LEN_WND_MAP, &id_ack_buf, &save_len, BPF_ANY);
-
-                bpf_skb_change_tail(skb, ON_WIRE_SIZE, 0);      
-
-                data = (void *)(long)skb->data;
-                data_end = (void *)(long)skb->data_end;
-                save_skb = (vtl_pkt_t *)data;
-                if(save_skb + 1 > data_end)
-                        return TC_ACT_OK;
-                bpf_map_update_elem(&BUF_WND_MAP, &id_ack_buf, save_skb, BPF_ANY);
-
-                bpf_skb_change_tail(skb, last_len, 0);               
-        }
-        else {
-                save_len = ON_WIRE_SIZE;
-                bpf_map_update_elem(&LEN_WND_MAP, &id_ack_buf, &save_len, BPF_ANY);
-
-                save_skb = (vtl_pkt_t *)data;
-                if(save_skb + 1 > data_end)
-                        return TC_ACT_OK;
-                bpf_map_update_elem(&BUF_WND_MAP, &id_ack_buf, save_skb, BPF_ANY); 
-        }
-        
-        // Update seq num for the next packet
-        const uint16_t cur_win_size = (const uint16_t) *num_seq;
-        (*num_seq)++;
-        *num_seq %= WND_SIZE;
-        bpf_map_update_elem(&NUM_SEQ_MAP, &id_seq, num_seq, BPF_ANY);
-
-        bpf_clone_redirect(skb, skb->ifindex+1, 0);
-
-        int tx_num = 0;
-        if(id_ack_buf == WND_SIZE - 1 || save_len < ON_WIRE_SIZE) {
-                unsigned int cursor = 0, i = 0, j = 0, lost = 0;
-                do {
-                        bpf_vtl_start_timer(325); // ms
-                        
-                        tx_num++;
-                        if(tx_num > RETX_MAX)
-                                break;
-
-                        for(i = cursor; i < WND_SIZE; i++) { // ACKs checking loop
-
-                                if(i > cur_win_size)
-                                        break;
-
-                                id_ack_buf = i;
-                                
-                                sk_state = (struct sock_state_t *) bpf_map_lookup_elem(&ACK_WND_MAP, &id_ack_buf);
-                                if(!sk_state)
-                                        break;
-
-                                if(sk_state->event != RECV_ACK) {
-                                        lost = 1;
-                                        break;
-                                }
-                        }
-
-                        if(lost) {
-                                
-                                cursor = i;
-                                for(j = cursor; j < WND_SIZE; j++) { // Retx loop
-
-                                        if(j > cur_win_size)
-                                                break;
-
-                                        id_ack_buf = j;
-
-                                        vtl_pkt_t *get_skb = (vtl_pkt_t *) bpf_map_lookup_elem(&BUF_WND_MAP, &id_ack_buf);
-                                        if(!get_skb)
-                                                break;
-
-                                        uint16_t* get_len = (uint16_t *) bpf_map_lookup_elem(&LEN_WND_MAP, &id_ack_buf);
-                                        if(!get_len)
-                                                break;
-
-                                        if(*get_len == ON_WIRE_SIZE)
-                                                bpf_skb_change_tail(skb, ON_WIRE_SIZE, 0);
-                                        
-                                        bpf_vtl_store_bytes(skb, 0, get_skb, ON_WIRE_SIZE, 0);
-
-                                        if(*get_len < ON_WIRE_SIZE)
-                                                bpf_skb_change_tail(skb, last_len, 0);
-
-                                        bpf_clone_redirect(skb, skb->ifindex+1, 0);
-                                }
-                                lost = 0;
-                        }
-                        else {//All pkts are ACKs
-                                lost = 0;
-                                break;
-                        }
-                } while(true);
-        }
-
-        return TC_ACT_SHOT;
+    return TC_ACT_SHOT;
 }
 
 SEC("listener_tf_sec")
 int _listener_tf(struct xdp_md *ctx) {
+    int index = 0;
+    negotiation_state nego_state = N_IDLE;
 
-        int index = 0;
-        struct sock_state_t *sk_state = NULL;
-        void *data = (void *)(long)ctx->data;
-        void *data_end = (void *)(long)ctx->data_end;
+    void *data = (void *)(long)ctx->data;
+    void *data_end = (void *)(long)ctx->data_end;
 
-        struct ethhdr *eth = (struct ethhdr *)data;
-        if(eth + 1 > data_end)
-                return XDP_PASS;
+    struct ethhdr *eth = (struct ethhdr *)data;
+    if(eth + 1 > data_end){
+        bpf_printk("listener ETH layer: malformed header.\n");
+        return XDP_DROP;
+    }
 
-        struct iphdr *iph = (struct iphdr *)(eth + 1);
-        if(iph + 1 > data_end)
-                return XDP_PASS;
+    struct iphdr *iph = (struct iphdr *)(eth + 1);
+    if(iph + 1 > data_end){
+        bpf_printk("listener IP layer: malformed header.\n");
+        return XDP_DROP;
+    }
 
-        if(iph->protocol != IPPROTO_VTL)
-                return XDP_PASS;
-
-        vtl_hdr_t *vtlh  = (vtl_hdr_t *)(iph + 1);
-        if(vtlh + 1 > data_end)
-                return XDP_PASS;
-
-        index = (int) vtlh->seq_num;
-        sk_state = (struct sock_state_t *) bpf_map_lookup_elem(&ACK_WND_MAP, &index);
-        if(!sk_state)
-                return XDP_PASS;
-
-        if(vtlh->pkt_type == NACK)
-                sk_state->event = IDLE;
-        else
-                sk_state->event = RECV_ACK;
-
-        long ret = bpf_map_update_elem(&ACK_WND_MAP, &index, sk_state, BPF_ANY);
-        if(ret != 0)
-                return XDP_PASS;
-
+    if(iph->protocol != IPPROTO_VTL)
         return XDP_PASS;
+
+    vtl_hdr_t *vtlh  = (vtl_hdr_t *)(iph + 1);
+    if(vtlh + 1 > data_end){
+        bpf_printk("listener VTL layer: malformed header.\n");
+        return XDP_DROP;
+    }
+
+    if(vtlh->pkt_type == NEGO_ACK) {
+        nego_state = N_ACCEPT;
+        bpf_printk("NEGO_ACK received !\n");
+    }
+    else if(vtlh->pkt_type == NEGO_NACK) {
+        nego_state = N_REFUSE;
+        bpf_printk("NEGO_NACK received !\n");
+    }
+    // No need "else". The receiver will not send ACK unless for negotiation.
+
+    long ret = bpf_map_update_elem(&QOS_NEGO_MAP, &index, &nego_state, BPF_ANY);
+    if(ret != 0) {
+        bpf_printk("Unable to update negotiation map.\n");
+        return XDP_DROP;
+    }
+
+    return XDP_DROP; // Let it for the kern network stack???
 }
 
 char _license[] SEC("license") = "GPL";
--- XTFsPool/ingress_test_graft.c
+++ XTFsPool/ingress_test_graft.c
@@ -1,8 +1,7 @@
-/* SPDX-License-Identifier: GPL-2.0 */
-
-//INGRESS GOBACKN ARQ GRAFT
+// INGRESS CANO GRAFT
 
 #include <stdint.h>
+#include <linux/if_ether.h>
 #include <netinet/ip.h>
 #include <linux/bpf.h>
 
@@ -11,98 +10,71 @@
 
 #include "../include/vtl.h"
 
-#define WND_SIZE                16
-
 struct bpf_map_def SEC("maps") xsks_map = {
-        .type           = BPF_MAP_TYPE_XSKMAP,
-        .key_size       = sizeof(int),
-        .value_size     = sizeof(int),
-        .max_entries    = 64,
+    .type = BPF_MAP_TYPE_XSKMAP,
+    .key_size = sizeof(int),
+    .value_size = sizeof(int),
+    .max_entries = 64,
 };
 
-struct bpf_map_def SEC("maps") NUM_ACK_MAP = {
-        .type           = BPF_MAP_TYPE_ARRAY,
-        .key_size       = sizeof(unsigned int),
-        .value_size     = sizeof(uint16_t),
-        .max_entries    = 1,
+struct bpf_map_def SEC("maps") QOS_NEGO_MAP = {
+        .type = BPF_MAP_TYPE_ARRAY,
+        .key_size = sizeof(unsigned int),
+        .value_size = sizeof(negotiation_state),
+        .max_entries = 1,
 };
 
-static __always_inline void swap_src_dst_mac(void *data) {
-    
-        unsigned short *p = data;
-        unsigned short dst[3];
-
-        dst[0] = p[0];
-        dst[1] = p[1];
-        dst[2] = p[2];
-        p[0] = p[3];
-        p[1] = p[4];
-        p[2] = p[5];
-        p[3] = dst[0];
-        p[4] = dst[1];
-        p[5] = dst[2];
-}
+struct bpf_map_def SEC("maps") GRAFT_ID_MAP = {
+    .type = BPF_MAP_TYPE_ARRAY,
+    .key_size = sizeof(unsigned int),
+    .value_size = sizeof(int8_t),
+    .max_entries = 256, 
+};
 
 SEC("ingress_tf_sec")
-int xdp_sock_prog(struct xdp_md *ctx) {
-
-        int index = 0, xsk_index = ctx->rx_queue_index;
-        uint16_t *ack_num = NULL;
+int xdp_sock_prog(struct xdp_md *ctx)
+{
+    int index = ctx->rx_queue_index, id = 0/*, p_time = 0*/;
+    int ret;
 
         void *data = (void *)(long)ctx->data;
         void *data_end = (void *)(long)ctx->data_end;
 
         struct ethhdr *eth = (struct ethhdr *)data;
-        if (eth + 1 > data_end)
-                return XDP_DROP;
+        if (eth + 1 > data_end){
+            bpf_printk("ETH: malformed header.\n");
+            return XDP_DROP;
+        }
 
         struct iphdr *iph = (struct iphdr *)(eth + 1);
-        if (iph + 1 > data_end)
-                return XDP_DROP;
+        if (iph + 1 > data_end){
+            bpf_printk("IPH: malformed header.\n");
+            return XDP_DROP;
+        }
 
-        if(iph->protocol != IPPROTO_VTL) {
-                return XDP_PASS;
+        if(iph->protocol != IPPROTO_VTL){
+            return XDP_PASS;
         }
 
         vtl_hdr_t *vtlh = (vtl_hdr_t *)(iph + 1);
-        if(vtlh + 1 > data_end)
-                return XDP_DROP;
-
-        ack_num = (uint16_t *) bpf_map_lookup_elem(&NUM_ACK_MAP, &index);
-        if(!ack_num)
-                return XDP_PASS;
-
-        uint16_t recv_csum = vtlh->checksum;
-
-        uint16_t compute_csum = 0;
-        if(vtl_csum(data, data_end, &compute_csum) == -1)
-                return XDP_PASS;
-
-        if(recv_csum == compute_csum && 
-                *ack_num == vtlh->seq_num) {
-
-                if (bpf_map_lookup_elem(&xsks_map, &xsk_index)) {
-
-                        (*ack_num)++;
-                        *ack_num %= WND_SIZE;
-                        bpf_map_update_elem(&NUM_ACK_MAP, &index, ack_num, BPF_ANY);
-
-                        return bpf_redirect_map(&xsks_map, xsk_index, 0);
-                }
+        if(vtlh + 1 > data_end){
+            bpf_printk("VTLH: malformed header.\n");
+            return XDP_DROP;
         }
-        else {
-                swap_src_dst_mac(eth);
-                __be32 temp_ip = iph->saddr;
-                iph->saddr = iph->daddr;
-                iph->daddr = temp_ip;
 
-                vtlh->pkt_type = NACK;
+        // Pass the required graft id to the cbr component
+        if(vtlh->gid > 0) {
+            ret = bpf_map_update_elem(&GRAFT_ID_MAP, &id, &(vtlh->gid), BPF_ANY);
+            if(ret != 0) {
+                bpf_printk("Could not update GRAFT_ID_MAP.\n");
+            }
 
-                // Trim NACK packet. TODO: add to ACK and update ip.len
-                bpf_xdp_adjust_tail(ctx, -vtlh->payload_len);
-
-                return XDP_TX;
+            return XDP_DROP;
         }
+    
+        // Else, there is no need for nego. Just pass data to appli
+        if (bpf_map_lookup_elem(&xsks_map, &index))
+            return bpf_redirect_map(&xsks_map, index, 0);
 
         return XDP_PASS;
 }
